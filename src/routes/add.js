98e65d0dcb81ac6395cf1c62eb08d7b6e0c8b359
S. Suprun <simonsuprun@gmail.com>
2020-03-03 14:38:12 +0300

    Add date parsing error handling

--- src/routes/add.js
+++ src/routes/add.js
@@ -2,7 +2,7 @@ const scheduler = require('node-schedule')
 const uuid = require('uuidv4')
 const chrono = require('chrono-node')
 
-const { BOT_ANSWER_CREATED, BOT_REMINDER_TEXT } = require('../config')
+const { BOT_ANSWER_CREATED, BOT_REMINDER_TEXT, BOT_ANSWER_PARSE_ERROR } = require('../config')
 const { formatString } = require('../utils')
 
 module.exports = (msg) => {
@@ -10,6 +10,12 @@ module.exports = (msg) => {
   const time = msg.match[2]
 
   const cron = chrono.parseDate(time)
+
+  if (!cron) {
+    msg.send(formatString(BOT_ANSWER_PARSE_ERROR, { time }))
+
+    return
+  }
 
   const job = scheduler.scheduleJob(
     cron,
